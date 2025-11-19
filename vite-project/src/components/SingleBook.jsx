249b93796b20e909b0f233ecd4d48fa58dc5470f
Leon35-fc <leon.f35@alice.it>
2025-11-19 16:59:42 +0100

    Update SingleBook.jsx

--- vite-project/src/components/SingleBook.jsx
+++ vite-project/src/components/SingleBook.jsx
@@ -1,24 +1,34 @@
+import { Component } from "react";
 import { Card, Col } from "react-bootstrap";
 
-function SingleBook (props) {
+class SingleBook extends Component {
 
-    console.log('SINGLE BOOK', props);
-    
+    state = {
+        selected : false
+    }
+
+    handleClick () {
+        if(this.setState()){
+            this.state = 'border border-warning'
+        } else {''}
+    }
+
+    render(props){
 
     return( 
         <Col xs={12} sm={6} md={4} lg={4}>
-        <Card key={props.book.id}  className="bg-primary shadow mb-3 me-2 h-100 w-75">
+        <Card key={props.book.id}  onClick={this.state} className="bg-primary shadow mb-3 me-2 h-100 w-75 {value}">
         <Card.Img variant='top' src={props.book.img} />
         <Card.Body>
         <Card.Title>{props.book.title}</Card.Title>
         <Card.Text>
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
+        Book description
         </Card.Text>
         </Card.Body>
         </Card>
         </Col>
     )
+    }
 }
 
 export default SingleBook
